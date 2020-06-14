d617f5c76c576b6dc21df69a1508e9eb5b5e2776
hansputera <47862061+hansputera@users.noreply.github.com>
2020-06-14 13:29:50 +0700

    yamaap

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const client = new Discord.Client({ disableMentions: true })
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
 });
