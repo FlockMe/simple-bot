5e99359d5c714b5fe775d82475a3eaabbbae1c47
RanggaGultom <41747668+RanggaGultom@users.noreply.github.com>
2020-06-14 13:25:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-module.exports = require('./src/index');
+const Discord = require('discord.js')
+const client = new Discord.Client()
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+});
+const config = require("./config.json")
+client.on('message', async message => {
+const prefix = config.prefix
+    let msg = message.content.toLowerCase();   
+    let args = message.content.slice(prefix.length).trim().split(" ");
+    let cmd = args.shift().toLowerCase();
+    let command = cmd;
+    
+    if(!message.content.startsWith(prefix)) return;
+    message.prefix = prefix;
+
+   if (!msg.startsWith(prefix)) return;
+   if(message.channel.type === "dm") return;
+   if (message.author.bot) return; 
+let commandFile = require(`./commands/${cmd}.js`);
+    commandFile.run(client, message, args);
+})
+client.login(process.env.TOKEN)
