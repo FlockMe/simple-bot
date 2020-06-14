afde98c601fbfa697688e8306115d5b8e777902a
Sinomaru <65635989+Sinomaru@users.noreply.github.com>
2020-06-14 13:34:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client({ disableMentions: true })
+const config = require("./config.json")
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -9,7 +10,6 @@ let status = [``,``]
   let statuss = status[Math.floor(Math.random() * status.length)];
   client.user.setActivity(statuss, {type: "WATCHING"})}, 4000) //isi angka terserah
 });
-const config = require("./config.json")
 client.on('message', async message => {
 const prefix = config.prefix
     let msg = message.content.toLowerCase();   
