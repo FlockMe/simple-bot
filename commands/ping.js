66e661d5b637fa8623f5ca72e8b3deb793a635d0
RanggaGultom <41747668+RanggaGultom@users.noreply.github.com>
2020-06-14 13:36:29 +0700

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js") 
+exports.run = async (client, message) => {
+  const m = await message.channel.send('Getting info...')
+  
+  const pembed = new Discord.MessageEmbed()
+  .setTitle(":ping_pong: PONG!")
+  .addField("Roundtrip took", `${m.createdTimestamp - message.createdTimestamp}ms`, true)
+  .addField("Hearthbeat", `${Math.round(client.ws.ping)}ms`, true)
+  .setColor("YELLOW")
+  .setFooter("Messa Bot by rnggadosen._")
+  return setTimeout(function(){ m.edit("Success getting info!", pembed);}, 3000);
+};
